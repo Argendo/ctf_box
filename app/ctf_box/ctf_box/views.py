af4bba2672e17142c27be25c5f496bff09e398a5
argend0 <a1@argend0.local>
2020-03-13 18:05:24 +0300

    fixed

--- app/ctf_box/ctf_box/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect
-
-def redirect_board(request):
-    return redirect('board/', permanent=True)
-
--- app/ctf_box/reg/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-urlpatterns = [
-    path('login/', login, name='login_url'),
-    path('logout/', logout, name='logout_url'),
-    path('register/', register)
-    ]
